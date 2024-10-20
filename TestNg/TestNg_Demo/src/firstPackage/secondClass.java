28e382ea4ac5715a457ca5cb6895b9dafcc9d67c
Onkar Malawade <132775903+OnkarMalawade@users.noreply.github.com>
2024-10-20 11:50:23 +0530

    Add files via upload

---
+++ TestNg/TestNg_Demo/src/firstPackage/secondClass.java
@@ -0,0 +1,14 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class secondClass {
+	@Test
+	  public void firstMethod() {
+		  System.out.println("This is 2 First Method of Packge 1");
+	  }
+	  @Test
+	  public void secondTestMethod() {
+		  System.out.println("This is 2 Second Method of Packge 1");
+	  }
+}
